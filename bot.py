e8d23ab7dd392304c80e834b93cadddd1bd88586
Thomas <tdevries4i@gmail.com>
2020-11-08 08:06:51 -0500

    Final Commit

--- bot.py
+++ bot.py
@@ -20,12 +20,6 @@ class MemberBox(discord.Client):
 				self.openBoxes[guild.name] = i
 				return True
 		return False
-		
-	def lookup_role(self, target, guild):
-		for i in guild.roles:
-			if str(i) == target:
-				return True
-		return False
 
 	def cache(self, g, role):
 		in_f = open("cache.json")
@@ -39,13 +33,17 @@ class MemberBox(discord.Client):
 		cache = json.dump(cache, out_f)
 		out_f.close()
 
-	def createBoxId(self, g, i):
+	def createBoxId(self, g, s, i):
+		flag = True
 		in_f = open("cache.json")
 		cache = json.load(in_f)
 		in_f.close()
-		for value in cache[g]:
-			if value == 0:
-				break
+		if not (str(g) in cache):
+			return f"Box {i}{s}"
+		for value in cache[str(g)]:
+			if value == f"Box {i}{s}":
+				return self.createBoxId(g, s, i + 1)
+			return f"Box {i}{s}"
 
 	def getFromCache(self, k1, k2):
 		cache = {}
@@ -80,8 +78,8 @@ class MemberBox(discord.Client):
 		await message.channel.send(f"Box \'{self.openBox}\' created")
 
 	async def closeBox(self, message, flags):
+		option = '-MB'
 		guild = message.author.guild
-		NBset = False
 		# Makes sure there is an open box
 		if not self.isBoxOpen(guild):
 			await message.channel.send('There is no open box. Try opening a box with `MemberBox create-box`')
@@ -92,12 +90,11 @@ class MemberBox(discord.Client):
 			for flag in flags:
 				print(flag)
 				if flag[:3] == '-t\"' and flag[len(flag) - 1] == '\"':
-					NewBoxName = f'Box {len(guild.roles)}: {flag[3 : len(flag) - 1]}-MB'
+					option = f': {flag[3 : len(flag) - 1]}-MB'
 					NBset = True
 				elif flag[:3] == '-t\"':
 					await message.channel.send(' Spaces are not allowed in your tag (-t)')
-		if not NBset:
-			NewBoxName = f'Box {len(guild.roles)}-MB'
+		NewBoxName = self.createBoxId(guild, option, 1)
 		# Creates the new role and saves the value
 		await self.openBoxes[guild.name].edit(name=NewBoxName)
 		self.cache(guild, self.openBoxes[guild.name])
@@ -117,7 +114,10 @@ class MemberBox(discord.Client):
 			if lst[i] == 'delete-box':
 				while lst[i][len(lst[i]) - 3:] != '-MB':
 					i += 1
-					target += lst[i] + ' '
+					try:
+						target += lst[i] + ' '
+					except:
+						pass
 				break
 		target = target[:len(target) - 1]
 		boxKey = self.getFromCache(str(guild),target)
@@ -130,7 +130,11 @@ class MemberBox(discord.Client):
 		await guild.get_role(boxKey).delete()
 		await message.channel.send(f'Box `{target}` destroyed')
 
-	async def manageFlags(self, flags):
+	async def manageHelp(self, source, message, flags):
+		guild = message.author.guild
+		for value in flags:
+			if value == '-help': 
+				await message.channel.send(source.__doc__)
 		print(flags)
 
 	# EVENT FUNCTIONS
@@ -138,6 +142,12 @@ class MemberBox(discord.Client):
 		print('Logged on as', self.user)
 
 	async def on_message(self, message):
+		""" Welcome to MemberBox, To create a box use `MemberBox create-box`.
+		
+		Use `MemberBox close-box` to seal it. The Flag `-t"tag"` can be used to assign a tag to the Box.
+		
+		`delete-box [Box Name]` will remove a box and all of it's members.
+		"""
 		print(message)
 		print(message.content)
 		flags = []
@@ -163,7 +173,7 @@ class MemberBox(discord.Client):
 			elif comand == 'delete-box':
 				await self.deleteBox(message, flags)
 			elif flags != None:
-				await self.manageFlags(flags)
+				await self.manageHelp(self.on_message, message, flags)
 			else:
 					await message.channel.send(f'The comand ```{comand}``` is not recognized for help. Try ```MemberBox -help for more information```')
 	
