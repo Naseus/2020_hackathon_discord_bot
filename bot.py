50f17039080e21ed0ac7a58f492616b99011085d
Thomas <tdevries4i@gmail.com>
2020-11-07 20:24:09 -0500

    Finishing create-box

--- bot.py
+++ bot.py
@@ -3,36 +3,68 @@ import discord
 # File handling
 f = open("secret.json")
 secrets = json.load(f)
+f.close()
 
 # Main MemberBox code
 class MemberBox(discord.Client):
 	# Constructor
 	def __init__(self):
-		self.boxes = []
-		self.isBoxOpen = False;
+		self.boxes = [[]]
+		self.openBox = 'Open Box-MB'
+		self.openBoxes = []
 		super(MemberBox, self).__init__()
 
 	# HELPER FUNCTIONS
+	def isBoxOpen(self, guild):
+		for i in guild.roles:
+			if str(i) == self.openBox:
+				return True
+		return False
+	def lookupRoleID(self, target, guild):
+		for role in guild.roles:
+			print(role.id)
+
 	async def createBox(self, message, flags):
-		boxName = 'Open Box'
-		self.isBoxOpen = True;
+		guild = message.author.guild
+		if self.isBoxOpen(guild):
+			await message.channel.send(f'The box {self.openBox}. Try closing the box first with `MemberBox close-box`')
+			return
+		print(self.isBoxOpen(guild))
+		self.openBox = 'Open Box-MB'
+		guild = message.author.guild
+		newBox = await guild.create_role(name=self.openBox)
+		self.openBoxes.append(newBox)
 		print(message)
 		print(flags)
-		await message.channel.send(f"Box \'{boxName}\' created")
+		await message.channel.send(f"Box \'{self.openBox}\' created")
 
 	async def closeBox(self, message, flags):
-		if self.isBoxOpen == False:
+		guild = message.author.guild
+		NBset = False
+		# Makes sure there is an open box
+		if not self.isBoxOpen(guild):
 			await message.channel.send('There is no open box. Try opening a box with `MemberBox create-box`')
 			return
-		self.isBoxOpen = False;
-		boxName = 'Open Box'
-		NewBoxName = f'Box{len(self.boxes)}'
-		print(message)
-		print(flags)
-		await message.channel.send(f'Box {boxName} closed: {NewBoxName}')
+		# Sets name of the new role
+		if flags != None:
+			for flag in flags:
+				if flag[:3] == '-n\"' and flag[len(flag) - 1] == '\"':
+					NewBoxName = f'{flag[3:len(flag)]}-MB'
+					NBset = True
+		if not NBset:
+			NewBoxName = f'Box{len(self.boxes)}'
+		# Creates the new role and saves the value
+		guild = message.author.guild
+		await guild.get_role(self.openBox).update(name=NewBoxName)
+		newBox = NewBoxName
+		self.boxes.append(newBox)
+		await message.channel.send(f'Box {self.openBox} closed: {NewBoxName}')
+		# Reset open box
+		self.openBox = ''
+
 
 	async def deleteBox(self, message, flags):
-		boxName = 'Open Box'
+		boxName = ''
 		print(message)
 		print(flags)
 		await message.channel.send(f'Box {boxName} destroyed')
@@ -64,12 +96,15 @@ class MemberBox(discord.Client):
 				await self.closeBox(message, flags)
 			elif comand == 'delete-box':
 				await self.deleteBox(message, flags)
-			elif flags[0] != NULL:
+			elif flags[0] != None:
 				await self.manageFlags(flags)
 			else:
 					await message.channel.send(f'The comand ```{comand}``` is not recognized for help. Try ```MemberBox -help for more information```')
+	
+	async def on_new_member_join(self, member):
+		if self.isBoxOpen(guild) == True:
+			await message.channel.send(self.openBox)
 
 # End of class
-
 client = MemberBox()
 client.run(secrets['bot_token'])
